551870b2a76ae4af9179f208d3120d8516f2b7b1
Scolopax0314 <13c20@csh-h.gne.go.kr>
2024-01-31 17:21:02 +0900

    d

--- Assets/scripts/moving.cs
+++ Assets/scripts/moving.cs
@@ -16,6 +16,13 @@ public class moving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        Vector2 direction = (mousePosition - transform.position).normalized;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
     }
 
