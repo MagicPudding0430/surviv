8f8cf11b1c0bb2d62d9c1036bf81748d8bb72008
jeajune <129724958+jeajune@users.noreply.github.com>
2024-01-31 20:44:18 +0900

    총알 슈슉

---
+++ Assets/scripts/GunCtrl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunCtrl : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform sPoint;
+    public float timeBetweenShots;
+
+    private float shotTime;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = rotation;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(Time.time >= shotTime)
+            {
+                Instantiate(bullet, sPoint.position, Quaternion.AngleAxis(angle - 90, Vector3.forward));
+            }   shotTime = Time.time + timeBetweenShots;
+        }
+    }
+}
